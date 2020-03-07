bcabcb8d88450bff3a2d90774018f1d43f656da2
caterineruiz <12345>
2020-03-07 15:28:38 -0500

    modificacion del splash, se agrego iconos de sitios

--- app/src/main/java/brayan/rivera/whitecity/Splash.java
+++ app/src/main/java/brayan/rivera/whitecity/Splash.java
@@ -4,27 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Splash extends AppCompatActivity {
 
+    ImageView imagen;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        imagen=findViewById(R.id.imgsplash); //referenciamos
 
-        TimerTask actividad = new TimerTask() {
+        // creacion del Glide
+
+        Glide.with(this).load("https://blogs.elespectador.com/wp-content/uploads/2017/04/torre-del-reloj-300x199.jpg").into(imagen);
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intento = new Intent(Splash.this,MainActivity.class);
-                startActivity(intento);
+                Intent pasar=new Intent(Splash.this,MainActivity.class);
+                startActivity(pasar);
                 finish();
             }
-        };
-        Timer tiempo = new Timer();
-        tiempo.schedule(actividad,2000);
+        },2000);
+
     }
 }
+
+
