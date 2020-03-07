a908ada19f7743a85aba80851fd49e8c0235cd10
Juanita Rivera <12345>
2020-03-07 16:20:54 -0500

    PETICION DE PERMISOS EN EL SPLASH

--- app/src/main/java/brayan/rivera/whitecity/Splash.java
+++ app/src/main/java/brayan/rivera/whitecity/Splash.java
@@ -1,41 +1,48 @@
 package brayan.rivera.whitecity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.widget.ImageView;
-
-import com.bumptech.glide.Glide;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Splash extends AppCompatActivity {
-
-    ImageView imagen;
-
+    final private  int REQUEST_CODE_ASK_PERMISSION = 111;
+    int permisoInternet, permisoLocalisacion, permisoCoarseLocalisacion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        imagen=findViewById(R.id.imgsplash); //referenciamos
+        solicitarPermisos();
 
-        // creacion del Glide
-
-        Glide.with(this).load("https://blogs.elespectador.com/wp-content/uploads/2017/04/torre-del-reloj-300x199.jpg").into(imagen);
-        new Handler().postDelayed(new Runnable() {
+        TimerTask actividad = new TimerTask() {
             @Override
             public void run() {
-                Intent pasar=new Intent(Splash.this,MainActivity.class);
-                startActivity(pasar);
+                Intent intento = new Intent(Splash.this,MainActivity.class);
+                startActivity(intento);
                 finish();
             }
-        },2000);
+        };
+        Timer tiempo = new Timer();
+        tiempo.schedule(actividad,2000);
+    }
+    private void solicitarPermisos() {
 
+        permisoInternet = ActivityCompat.checkSelfPermission(Splash.this, Manifest.permission.INTERNET);
+        permisoLocalisacion = ActivityCompat.checkSelfPermission(Splash.this, Manifest.permission.ACCESS_FINE_LOCATION);
+        permisoCoarseLocalisacion = ActivityCompat.checkSelfPermission(Splash.this, Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        if ( permisoInternet != PackageManager.PERMISSION_GRANTED || permisoLocalisacion != PackageManager.PERMISSION_GRANTED || permisoCoarseLocalisacion!= PackageManager.PERMISSION_GRANTED){
+            if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ) {
+                requestPermissions(new String[]{Manifest.permission.INTERNET, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_CODE_ASK_PERMISSION);
+            }
+        }
     }
 }
-
-
