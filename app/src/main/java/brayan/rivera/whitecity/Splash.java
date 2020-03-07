703c3f5fbfcb0f2ff804fd50948324195ee8480f
Juanita Rivera <12345>
2020-03-07 11:39:13 -0500

    se modifco el splash

--- app/src/main/java/brayan/rivera/whitecity/Splash.java
+++ app/src/main/java/brayan/rivera/whitecity/Splash.java
@@ -2,7 +2,11 @@ package brayan.rivera.whitecity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Splash extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        TimerTask actividad = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intento = new Intent(Splash.this,MainActivity.class);
+                startActivity(intento);
+                finish();
+            }
+        };
+        Timer tiempo = new Timer();
+        tiempo.schedule(actividad,2000);
     }
 }
